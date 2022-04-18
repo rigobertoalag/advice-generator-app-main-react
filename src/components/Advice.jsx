f80a1fb566b421237c264dc8a94eb759042099b6
rigobertoalag <a20150338@utem.edu.mx>
2022-04-18 11:57:27 -0500

    Finish

--- src/components/Advice.jsx
+++ src/components/Advice.jsx
@@ -1,27 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./advice.scss";
 
 import divider from "../images/pattern-divider-desktop.svg";
 import dice from "../images/icon-dice.svg";
 
 const Advice = () => {
+  const [advice, setAdvice] = useState();
+
+  const fetchAdvise = async () => {
+    await fetch("https://api.adviceslip.com/advice")
+    .then((response) => response.json())
+    .then((advice) => setAdvice(advice));
+  };
+
+  useEffect(() => {
+    fetchAdvise()
+  }, []);
+
   return (
     <div className="advice">
-      <p className="ad-number">ADVICE #666</p>
+      {advice ? (
+        <>
+          <p className="ad-number">A D V I C E # {advice.slip.id}</p>
 
-      <div className="ad-body">
-        <p className="ad-body-text">aqui va el advise</p>
-      </div>
+          <div className="ad-body">
+            <p className="ad-body-text">"{advice.slip.advice}"</p>
+          </div>
 
-      <div className="divider-container">
-        <span className="divider"></span>
-        <img src={divider} alt="divider" className="divider-pattern" />
-        <span className="divider"></span>
-      </div>
+          <div className="divider-container">
+            <span className="divider"></span>
+            <img src={divider} alt="divider" className="divider-pattern" />
+            <span className="divider"></span>
+          </div>
 
-      <div className="random-btn">
-        <img src={dice} alt="dice" className="dice-pattern" />
-      </div>
+          <div className="random-btn" onClick={()=>fetchAdvise()}>
+            <img src={dice} alt="dice" className="dice-pattern" />
+          </div>
+        </>
+      ) : (
+        <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+      )}
     </div>
   );
 };
